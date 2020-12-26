31cc8c6a37232a7171bcb2b77442d3b6ac275a35
Colette <colette.rs.shaw@gmail.com>
2020-12-26 14:06:03 +0000

    Add schema; write more queries

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from google.cloud import bigquery
 
 client = bigquery.Client()
 
+# SCHEMA:
+# date	            INTEGER	    NULLABLE	
+# channelGrouping	STRING	    NULLABLE	
+# deviceCategory	STRING	    NULLABLE	
+# sessions	        INTEGER	    NULLABLE	
+# conversions	    INTEGER	    NULLABLE	
+
 testQuery = """
     SELECT * FROM `apprenticeship-299321.sample_data.datatable`
     LIMIT 10
@@ -11,7 +18,34 @@ query = """
     SELECT * FROM `apprenticeship-299321.sample_data.datatable`
 """
 
-results = client.query(query)
+query1 = """
+    SELECT COUNT(*) FROM `apprenticeship-299321.sample_data.datatable`
+"""
+
+query2 = """
+    SELECT deviceCategory, COUNT(*)
+    FROM `apprenticeship-299321.sample_data.datatable`
+    GROUP BY deviceCategory
+"""
+
+query3 = """
+    SELECT channelGrouping, COUNT(*)
+    FROM `apprenticeship-299321.sample_data.datatable`
+    GROUP BY channelGrouping
+"""
+
+query4 = """
+    SELECT date, COUNT(*)
+    FROM `apprenticeship-299321.sample_data.datatable`
+    GROUP BY date
+    ORDER BY date ASC
+"""
+
+# conversion rate for device category
+# conversion rate for channel grouping
+# conversion rate for date? maybe that's not useful
+
+results = client.query(query4)
 
 for row in results:
     print(str(row))
